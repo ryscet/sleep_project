d438dfefe62243b5594045433b4d38b8b08d6c0a
ryscet <cetnarski.ryszard@gmail.com>
2016-09-14 12:25:12 +0200

    finished hipnohgram comparison plot

--- intersect_hipno.py
+++ intersect_hipno.py
@@ -14,6 +14,8 @@ import numpy as np
 import seaborn as sns
 from collections import OrderedDict 
 
+stage_color_dict = {'N1' : 'royalblue', 'N2' :'forestgreen', 'N3' : 'coral', 'rem' : 'plum', 'wake' : 'y' }
+
 def get_hipnogram_intersection():
     psg_hipno = ph.parse_psg_stages()
     noo_hipno = ph.parse_neuroon_stages()
@@ -44,30 +46,40 @@ def get_hipnogram_intersection():
             stage_id = intersection.iloc[0, intersection.columns.get_loc('stage_name_neuro')] 
             # Keep appending results to a list stored in a dict. Check if the list exists, if not create it.
             if stage_id not in all_durations.keys():
-                all_durations[stage_id] = [int(duration)]
+                # Divide to get minutes
+                all_durations[stage_id] = [duration / 1000.0 / 60.0]
                 
-            all_durations[stage_id].append(int(duration))
+            all_durations[stage_id].append(duration / 1000.0 / 60.0)
     
+    means = OrderedDict()
+    stds = OrderedDict()
+    sums = OrderedDict()
     for key, value in all_durations.items():
-        all_durations[key] = np.array(value)
+        if key != 'wake':
+            means[key] = np.array(value).mean()
+            stds[key] = np.array(value).std()
+            sums[key] = np.array(value).sum()
         
-    results = preapre_results(all_durations)
+    #results = preapre_results(all_durations)
 
-
-        # Plot total time per stage
-    #sns.barplot(x = list(all_durations.keys()), y =list(all_durations.values()))
+    fig, axes = plt.subplots(1,2)
+    fig.suptitle('Neuroon-psg hinogram comparison', fontweight = 'bold')
     
-    return  results
-
+    axes[1].bar(left = [1,2,3], height = list(means.values()),width = 0.3, alpha = 0.8, align = 'center', color = [stage_color_dict['N2'], stage_color_dict['N3'], stage_color_dict['rem']], tick_label = list(means.keys()), edgecolor = 'black')
+    
+    (_, caps, _) = axes[1].errorbar([1,2,3], list(means.values()),  yerr= list(stds.values()), fmt='none', ecolor = 'black', alpha = 0.8, elinewidth = 0.8, linestyle = '-.')
+    
+    axes[1].set_xlim(0.49, 3.51)
+    axes[1].set_ylabel('Average hipnogram overlap in minutes')
+    axes[1].set_xlabel('sleep stage')
+    
+    axes[0].bar(left = [1,2,3], height = list(sums.values()),width = 0.3, alpha = 0.8, align = 'center', color = [stage_color_dict['N2'], stage_color_dict['N3'], stage_color_dict['rem']], tick_label = list(means.keys()), edgecolor = 'black')
+    axes[0].set_ylabel('Total hipnogram overlap in minutes')
+    
+    for cap in caps:
+        cap.set_color('black')
+        cap.set_markeredgewidth(1)
         
-def preapre_results(all_durations):
+    raise_window()
 
-    # Prepare the dataframe for storing the results
-    parsed_durations = pd.DataFrame(columns = ['total_intersect_duration', 'number_of_intersects', 'average_intersect_duration'])
-   # Iterate over dict containing list with all meetings durations
-    for key, value in all_durations.items():
-        # Convert the list to numpy array 
-        durations =np.array(value)
-        # Store results for the room as a row in a dataframe
-        parsed_durations.loc[key, :] =  [durations.sum() / 1000.0,  len(durations), durations.mean() / 1000.0]  
-    return parsed_durations
+    fig.savefig('figures/hipnogram_comparison/hipno_overlap.pdf')
